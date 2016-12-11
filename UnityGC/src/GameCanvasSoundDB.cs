da9d4d7fae4ddd38a156eec59dc1b631f182d43e
Seibe TAKAHASHI <t@seibe.jp>
2016-12-12 01:27:49 +0900

    #9 (On the way)

---
+++ UnityGC/src/GameCanvasSoundDB.cs
@@ -0,0 +1,29 @@
+﻿/*------------------------------------------------------------*/
+/// <summary>Sound Database</summary>
+/// <author>Seibe TAKAHASHI</author>
+/// <remarks>
+/// (c) 2015-2016 Smart Device Programming.
+/// This software is released under the MIT License.
+/// http://opensource.org/licenses/mit-license.php
+/// </remarks>
+/*------------------------------------------------------------*/
+using UnityEngine;
+
+namespace GameCanvas
+{
+    /// <summary>
+    /// 音声情報を保持するためのカスタムアセット
+    /// </summary>
+    public class SoundDatabase : ScriptableObject
+    {
+        /// <summary>
+        /// カスタムアセットの保存先
+        /// </summary>
+        public const string Path = "Assets/Plugins/UnityGC/Resources/GCSoundDB.asset";
+
+        /// <summary>
+        /// 音声リスト
+        /// </summary>
+        public AudioClip[] sounds;
+    }
+}
